4590f19353fd28a40e26fe003a5fe31992244eb4
kadir-zeren <k4dzeren@gmail.com>
2023-12-09 08:34:03 +0300

    v2

--- src/useState/UseStateCounter.jsx
+++ src/useState/UseStateCounter.jsx
@@ -1,11 +1,24 @@
+import { useState } from "react";
+
 const UseStateCounter = () => {
+  const [count, setCount] = useState(0);
+  const handleInc = () => {
+    setCount(count + 1);
+  };
+  const handleDec = () => {
+    if (count <= 0) {
+      alert("count can not be less than 0");
+    } else {
+      setCount(count - 1);
+    }
+  };
   return (
     <div>
       <h2>USE STATE HOOK</h2>
-      <h1>Count:0</h1>
-      <button>INC</button>
-      <button>CLR</button>
-      <button>DEC</button>
+      <h1>Count:{count}</h1>
+      <button onClick={handleInc}>INC</button>
+      <button onDoubleClick={() => setCount(0)}>CLR</button>
+      <button onClick={handleDec}>DEC</button>
     </div>
   );
 };
