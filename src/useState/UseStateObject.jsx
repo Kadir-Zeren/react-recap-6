985f0a04a70ab38dc481d3d0b1b16a643d3c3271
kadir-zeren <k4dzeren@gmail.com>
2023-12-09 09:39:20 +0300

    v4

--- src/useState/UseStateObject.jsx
+++ src/useState/UseStateObject.jsx
@@ -1,14 +1,33 @@
+import { useState } from "react";
+
 const UseStateObject = () => {
+  //   const [name, setName] = useState("Ahmet Can");
+  //   const [age, setAge] = useState(30);
+  //   const [salary, setSalary] = useState(50000);
+
+  const [kisi, setKisi] = useState({
+    name: "Ahmet Kan",
+    age: 30,
+    salary: 50000,
+  });
+
+  console.log(kisi);
   return (
     <div>
       <h1>USE STATE OBJECT </h1>
-      <h2>NAME</h2>
-      <h2>AGE:</h2>
-      <h2>JOB:</h2>
+      <h2>NAME:{kisi.name}</h2>
+      <h2>AGE:{kisi.age}</h2>
+      <h2>Salary:{kisi.salary}</h2>
 
-      <button>Change Name</button>
-      <button>inc age</button>
-      <button>inc salary</button>
+      <button onClick={() => setKisi({ ...kisi, name: "Mehmet Kan" })}>
+        Change Name
+      </button>
+      <button onClick={() => setKisi({ ...kisi, age: kisi.age + 1 })}>
+        inc age
+      </button>
+      <button onClick={() => setKisi({ ...kisi, salary: kisi.salary + 10000 })}>
+        inc salary
+      </button>
     </div>
   );
 };
